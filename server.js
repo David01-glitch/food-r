363fe753528d7c6541beeeeb50caa59d27c9e289
David01-glitch <grandkitchen@gmail.com>
2026-05-07 02:54:29 +0530

    Add Heroku deployment files (Node/Express static server)

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname, {
+  extensions: ['html'],
+  maxAge: '1h'
+}));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Grandma's Kitchen is serving on port ${PORT}`);
+});
